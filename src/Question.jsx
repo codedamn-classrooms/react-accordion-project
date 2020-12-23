ae01310260d1d06644fc2bac13fff0e19d0c84b6
Mehul Mohan <mehulmpt@gmail.com>
2020-12-23 20:49:27 +0530

    lab

--- src/Question.jsx
+++ src/Question.jsx
@@ -3,16 +3,19 @@ import React, { useState } from 'react'
 const Question = (props) => {
 	const { title, info } = props
 	// add a state variable here
+	const [visible, setVisible] = useState(false)
 
 	return (
 		<article className="question">
 			<header>
 				<h4>{title}</h4>
 				{/* make this toggle functionality onClick */}
-				<button className="btn">+</button>
+				<button className="btn" onClick={() => setVisible((state) => !state)}>
+					{visible ? '-' : '+'}
+				</button>
 			</header>
 			{/* make this render conditionally */}
-			<p>{info}</p>
+			{visible && <p>{info}</p>}
 		</article>
 	)
 }
