656e25422cb29da53dac99173f7a8e752de37700
Mehul Mohan <mehulmpt@gmail.com>
2020-12-23 20:44:29 +0530

    lab

--- src/Question.jsx
+++ src/Question.jsx
@@ -2,14 +2,16 @@ import React, { useState } from 'react'
 
 const Question = (props) => {
 	const { title, info } = props
+	// add a state variable here
 
-	// update the UI here
 	return (
 		<article className="question">
 			<header>
 				<h4>{title}</h4>
+				{/* make this toggle functionality onClick */}
 				<button className="btn">+</button>
 			</header>
+			{/* make this render conditionally */}
 			<p>{info}</p>
 		</article>
 	)
